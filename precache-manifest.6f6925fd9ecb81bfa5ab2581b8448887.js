b3c3a7faf6b9e53612c42e6e895b0458c134cb34
nik <chaudharynikhil7@gmail.com>
2020-09-20 00:12:40 +0530

    Updates

---
+++ precache-manifest.6f6925fd9ecb81bfa5ab2581b8448887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32dfe65d7350d960af5fb581398f2caf",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "d81547011da0a2cd1d00",
+    "url": "/covid-19/static/css/main.a2fa5e9d.chunk.css"
+  },
+  {
+    "revision": "36cd6f0f9676df6bc265",
+    "url": "/covid-19/static/js/2.dfa11f65.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-19/static/js/2.dfa11f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d81547011da0a2cd1d00",
+    "url": "/covid-19/static/js/main.9743f0b3.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
